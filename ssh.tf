c2455775c2119b1d90e78de0d99003026c3852cc
Kapitan <kapitanav@gmail.com>
2019-11-26 11:56:11 +0200

    Update ssh.tf

--- ssh.tf
+++ ssh.tf
@@ -8,12 +8,7 @@ resource "aws_security_group" "ssh" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
-        ingress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }  
+       
     egress {
         from_port   = 0
         to_port     = 0
